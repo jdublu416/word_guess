0f8f5f2fc0a9e71224c44da694ddb17f1ac5d0cd
Jdublu <>
2018-09-16 15:27:32 -0400

    update to files for app

--- letter.js
+++ letter.js
@@ -1,15 +1,28 @@
-function Letter (underlyingChar) {
-  this.correctlyGuessed =false;
-  this.underlyingChar=underlyingChar;
-  this.checkLetter = function(guessedLetter) {
-    if (guessedLetter === this.underlyingChar) {
-      //letter matches so the response will be
-      console.log("Your letter " + guessedLetter + " is correct!");
-      return true;
-    } else {
-      console.log("Bummer, you guessed incorrectly, Go again!");
-      return false;
-    }
-  };
+function Letter(char) {
+  // the test() method executes a search for a match between a regular expression
+  // and a specified string. Returns true or false (regExp).
+  this._isVisible = !/[a-z1-9]/i.test(char);
+  this._char = char;
 }
+
+Letter.toString = function() {
+  if (this._isVisible) {
+    return this._char;
+  } else {
+    return "_";
+  }
+};
+
+Letter.getSolution = function() {
+  return this.char;
+};
+
+Letter.guess = function(charGuess) {
+  if (charGuess.toUpperCase() === this.char.toUpperCase()) {
+    this._isVisible = true;
+    return true;
+  }
+  return false;
+};
+
 module.exports = Letter;
--- word.js
+++ word.js
@@ -1,19 +1,38 @@
-var Letter = require("./letter.js");
+var Letter = require("./letter");
 
-function Word(randomWord) {
-  this.randomWord = randomWord;
-  this.letterArray=[];
-  this.separateWord = function() {
-    var letterArray = randomWord.split("");
-    for (var i = 0; i < letterArray.length; i++) {
-        this.letterArray.push(new Letter(letterArray[i]));
-    }
-    console.log(this.letterArray);
-  };
-  this.checkGuess= function(){
-
-    
-  }
+function Word(word) {
+  // need to split the word into a letters array that returns as a new Letter object representing
+  //the letters of the underlying word
+  this.letters = word.split("").map(function(char) {
+    return new Letter(char);
+  });
 }
+
+//need to create a fx that returns a string representing a word calling on
+//each letter of object (first fx in Letter.js) diplays the character or an
+//underscore and concatenate those together.
+
+Word.getSolution = function() {
+  return this.letters
+    .map(function(letter) {
+      return letter.getSolution();
+    })
+    .join(""); //iterates over letter and then returns a string using .join()
+};
+// need a function that takes a character as an argument and calls the guess fx on each
+// letter obj(second function in Letter.js)
+
+Word.guessLetter = function(char) {
+  let foundLetter = false;
+  this.letters.forEach(function(letter) {
+    if (letter.guess(char)) {
+      foundLetter = true;
+    }
+  });
+  // Log the word guessed so far
+  console.log("\n" + this + "\n");
+  // return found a letter
+  return foundLetter;
+};
 
 module.exports = Word;
